b63b738e95421f6df6334b34398d36c1a943bfa6
raddysr <raddy.radev@gmail.com>
2021-12-09 10:20:25 +0200

    project update

--- personalportofolio/personalportfolio/personalportfolio/urls.py
+++ personalportofolio/personalportfolio/personalportfolio/urls.py
@@ -7,4 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home' ),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
